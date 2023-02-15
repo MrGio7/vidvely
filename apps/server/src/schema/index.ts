d546f421697246cf8290d818504eb7e7fd59921c
Giorgi Bakashvili <lashagiorgi1233@gmail.com>
2023-02-15 21:07:42 +0400

    added custom scalars

--- apps/server/src/schema/index.ts
+++ apps/server/src/schema/index.ts
@@ -1,15 +1,15 @@
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "@graphql-tools/schema";
+import { typeDefs as scalarTypeDefs } from "graphql-scalars";
 
 // types
-import scalars from "./scalars";
 import userType from "./user/user.type";
 
 // Resolvers
 import userResolver from "./user/user.resolver";
 
 const schema = makeExecutableSchema({
-  typeDefs: mergeTypeDefs([`type Query`, `type Mutation`, scalars, userType]),
+  typeDefs: mergeTypeDefs([`type Query`, `type Mutation`, ...scalarTypeDefs, userType]),
   resolvers: mergeResolvers(userResolver),
 });
 
