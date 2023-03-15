23f3482261392809b35dc8b77bddb71ff2d39373
Giorgi Bakashvili <lashagiorgi1233@gmail.com>
2023-03-15 21:28:32 +0400

    removed unneccessary file

--- apps/web/src/utils/meeting.ts
+++
@@ -1,9 +0,0 @@
-import { trpcProxy } from "./trpc";
-
-export async function getAttendee(chimeAttendeeId: string, externalUserId?: string) {
-  return {
-    name: await trpcProxy(token).getUserName.query({
-      userId: externalUserId || chimeAttendeeId,
-    }),
-  };
-}
